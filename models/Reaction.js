7aa8a6e32aac6384e5270357d865f8b9c327eff5
Potter <potteramylynn@gmail.com>
2023-05-30 15:09:21 -0500

    updated schemas

---
+++ models/Reaction.js
@@ -0,0 +1,30 @@
+const { Schema } = require('mongoose');
+
+const reactionSchema = new Schema (
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      max_length: 208,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  },
+)
+const Reaction = reactionSchema;
+module.exports = Reaction;
--- models/User.js
+++ models/User.js
@@ -1,9 +1,38 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require("./Thought")
+
+const userSchema = new Schema(
+  {
+    username: {
+      type: String, 
+      required: true,
+      unique: true,
+      max_length: 20,
+    },
+    email: {
+      type: String,
+      required: true,
+      max_length: 50,
+      validate: [
+        isEmail
+      ],
+      // match to validate email
+      match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please fill a valid email address'],
+    },
+    thoughts: {
+      reference: [thoughtSchema],
+    },
+    friends: [userSchema],
+  }, 
+  { 
+    // Create a virtual called friendCount that retrieves the length of the user's friends array field on query.
+    friendCount: {
+    virtuals: true,
+    }
+  }
+);
 
 
-username
 
-email
-
-thoughts
-
-friends
+const User = model("user", userSchema);
+module.exports = User;
