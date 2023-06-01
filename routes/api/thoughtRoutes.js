661cc390188d7be72400f90faaee9ed877e66a02
Potter <potteramylynn@gmail.com>
2023-06-01 13:16:48 -0500

    added reaction routes

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
-const {getThoughts, createThought, updateThought, deleteThought} = require("../../controllers/thoughtController");
+const {getThoughts, createThought, updateThought, deleteThought, addReaction, deleteReaction} = require("../../controllers/thoughtController");
 
 router.route("/").get(getThoughts).post(createThought)
 router.route("/:id").put(updateThought).delete(deleteThought);
+router.route("/api/thoughts/:thoughtId/reactions").post(addReaction).delete(deleteReaction);
 module.exports = router;
